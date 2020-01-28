187f077e9b52cc6bee62f0d4618dfaa04d71f6b5
Aditya <dtrg21@gmail.com>
2020-01-28 07:09:14 +0530

    Remove User Onboard field

--- priv/repo/migrations/20191213184706_add_user_onboard_to_users.exs
+++
@@ -1,9 +0,0 @@
-defmodule Hyperlog.Repo.Migrations.AddUserOnboardToUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table :users do
-      add :onboard_complete, :boolean, default: false
-    end
-  end
-end
