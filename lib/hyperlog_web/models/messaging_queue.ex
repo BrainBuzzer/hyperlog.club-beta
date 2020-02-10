bb13de67c515f273306028eb790fa8af976a2ee3
Aditya Giri <dtrg21@gmail.com>
2020-02-10 21:15:35 +0530

    Run Messaging Queue only on production

--- lib/hyperlog_web/models/messaging_queue.ex
+++ lib/hyperlog_web/models/messaging_queue.ex
@@ -1,23 +1,27 @@
 defmodule HyperlogWeb.MessagingQueue do
   def send_discord_token(token, user_id) do
-    {:ok, connection} = AMQP.Connection.open
-    {:ok, channel} = AMQP.Channel.open(connection)
+    if Mix.env() == :prod do
+      {:ok, connection} = AMQP.Connection.open
+      {:ok, channel} = AMQP.Channel.open(connection)
 
-    {:ok, send_data} = Poison.encode(%{access_token: token, user_id: user_id})
+      {:ok, send_data} = Poison.encode(%{access_token: token, user_id: user_id})
 
-    AMQP.Queue.declare(channel, "discord_tokens")
-    AMQP.Basic.publish(channel, "", "discord_tokens", send_data, [content_type: "application/json"])
-    AMQP.Connection.close(connection)
+      AMQP.Queue.declare(channel, "discord_tokens")
+      AMQP.Basic.publish(channel, "", "discord_tokens", send_data, [content_type: "application/json"])
+      AMQP.Connection.close(connection)
+    end
   end
 
   def send_role_data(user_id, roles, action) do
-    {:ok, connection} = AMQP.Connection.open
-    {:ok, channel} = AMQP.Channel.open(connection)
+    if Mix.env() == :prod do
+      {:ok, connection} = AMQP.Connection.open
+      {:ok, channel} = AMQP.Channel.open(connection)
 
-    {:ok, send_data} = Poison.encode(%{user_id: user_id, roles: roles, action: action})
+      {:ok, send_data} = Poison.encode(%{user_id: user_id, roles: roles, action: action})
 
-    AMQP.Queue.declare(channel, "discord_roles")
-    AMQP.Basic.publish(channel, "", "discord_roles", send_data, [content_type: "application/json"])
-    AMQP.Connection.close(connection)
+      AMQP.Queue.declare(channel, "discord_roles")
+      AMQP.Basic.publish(channel, "", "discord_roles", send_data, [content_type: "application/json"])
+      AMQP.Connection.close(connection)
+    end
   end
 end
