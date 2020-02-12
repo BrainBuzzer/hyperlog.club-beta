79ee46c70816f528b9f728857dc7f147844883ef
Aditya Giri <dtrg21@gmail.com>
2020-02-13 00:14:20 +0530

    Add GitHub token to messaging queue after connecting user with GitHub

--- lib/hyperlog_web/models/messaging_queue.ex
+++ lib/hyperlog_web/models/messaging_queue.ex
@@ -12,6 +12,17 @@ defmodule HyperlogWeb.MessagingQueue do
     end
   end
 
+  def send_github_token(token, user_email) do
+    {:ok, connection} = AMQP.Connection.open
+    {:ok, channel} = AMQP.Channel.open(connection)
+
+    {:ok, send_data} = Poison.encode(%{token: token, email: user_email})
+
+    AMQP.Queue.declare(channel, "github_tokens")
+    AMQP.Basic.publish(channel, "", "github_tokens", send_data, [content_type: "application/json"])
+    AMQP.Connection.close(connection)
+  end
+
   def send_role_data(user_id, roles, action) do
     if Mix.env() == :prod do
       {:ok, connection} = AMQP.Connection.open
